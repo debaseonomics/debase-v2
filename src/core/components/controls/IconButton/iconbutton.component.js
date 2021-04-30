4e252a5bf9fbf50de8feb6e4410c077aeebdbcaf
PunkUnknown <PunkUnknown@tutanota.com>
2021-04-30 21:18:20 +0500

    fixed icon links

--- src/core/components/controls/IconButton/iconbutton.component.js
+++ src/core/components/controls/IconButton/iconbutton.component.js
@@ -3,32 +3,31 @@ import React from 'react';
 import { StyledIconButton, StyledIcon } from './iconbutton.styles';
 
 const IconButton = ({
-    variant = 'default',
-    color = 'secundary',
-    size = 'medium',
-    edge = 'smooth',
-    isDisabled = false,
-    isLoading = false,
-    onClick,
-    children
+	variant = 'default',
+	color = 'secundary',
+	size = 'medium',
+	edge = 'smooth',
+	isDisabled = false,
+	isLoading = false,
+	href,
+	onClick,
+	children
 }) => {
-
-    return (
-        <StyledIconButton
-            variant={variant}
-            color={color}
-            size={size}
-            edge={edge}
-            isDisabled={isDisabled}
-            isLoading={isLoading}
-            onClick={onClick}
-        >   
-            <StyledIcon>
-                {children}
-            </StyledIcon>
-        </StyledIconButton>
-    );
-
+	return (
+		<StyledIconButton
+			variant={variant}
+			color={color}
+			size={size}
+			edge={edge}
+			isDisabled={isDisabled}
+			isLoading={isLoading}
+			onClick={onClick}
+			href={href}
+			target="_blank"
+		>
+			<StyledIcon>{children}</StyledIcon>
+		</StyledIconButton>
+	);
 };
 
 export default IconButton;
