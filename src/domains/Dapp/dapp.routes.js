414e1be7c17cd96e92a956c4af4c66e4103de2be
Jason Liu <jasonliudan@gmail.com>
2021-05-04 15:01:43 -0400

    update routes

--- src/domains/Dapp/dapp.routes.js
+++ src/domains/Dapp/dapp.routes.js
@@ -10,25 +10,25 @@ const DAPP_ROUTES = [
 	},
 	{
 		label: 'pools',
-		path: '/pools',
+		path: '/dashboard/pools',
 		icon: <AccountTreeIcon />,
 		component: <Pools />
 	},
 	{
 		label: 'rebase',
-		path: '/rebase',
+		path: '/dashboard/rebase',
 		icon: <TuneIcon />,
 		component: <Rebase />
 	},
 	{
 		label: 'roadmap',
-		path: '/roadmap',
+		path: '/dashboard/roadmap',
 		icon: <InsightsIcon />,
 		component: <Roadmap />
 	},
 	{
 		label: 'faq',
-		path: '/faq',
+		path: '/dashboard/faq',
 		icon: <QuestionAnswerIcon />,
 		component: <Faq />
 	}
