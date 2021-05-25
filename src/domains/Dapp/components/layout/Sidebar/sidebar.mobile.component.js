b6a76653db78fd695d816211f9c653e770f0e753
Okawa <okawanori001@gmail.com>
2021-05-25 03:13:55 -0400

    Mobile View

--- src/domains/Dapp/components/layout/Sidebar/sidebar.mobile.component.js
+++ src/domains/Dapp/components/layout/Sidebar/sidebar.mobile.component.js
@@ -93,32 +93,10 @@ const SidebarMobile = ({
                     trade degov
                 </Button>
             </div>
+            
             <StyledSocialList>
-                    {renderSocialList()}
-                </StyledSocialList>
-            {/* <StyledSection>
-                <Flexbox gap="15px">
-                    <Button
-                        as="a"
-                        href="https://app.uniswap.org/#/swap?inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f&outputCurrency=0x9248c485b0b80f76da451f167a8db30f33c70907"
-                        target="_blank"
-                    >
-                        trade debase
-                    </Button>
-                    <Button
-                        as="a"
-                        href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x469e66e06fec34839e5eb1273ba85a119b8d702f"
-                        target="_blank"
-                    >
-                        trade degov
-                    </Button>
-                </Flexbox>
-            </StyledSection> */}
-            {/* <StyledSection>
-                <StyledSocialList>
-                    {renderSocialList()}
-                </StyledSocialList>
-            </StyledSection> */}
+                {renderSocialList()}
+            </StyledSocialList>
         </StyledSidebar>
     );
 
